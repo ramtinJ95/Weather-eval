02338c0d885f4c0418d2dceec6048392f77afe7c
Ramtin Javanmardi <ramme95@gmail.com>
2026-02-24 21:02:51 +0100

    infra: enable foundational GCP APIs in bootstrap terraform

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -14,6 +14,21 @@ provider "google" {
   region  = var.region
 }
 
+locals {
+  foundation_apis = toset([
+    "cloudresourcemanager.googleapis.com",
+    "iam.googleapis.com",
+  ])
+}
+
+resource "google_project_service" "foundation" {
+  for_each = local.foundation_apis
+
+  project            = var.project_id
+  service            = each.value
+  disable_on_destroy = false
+}
+
 resource "google_storage_bucket" "terraform_state" {
   name                        = var.state_bucket_name
   location                    = var.state_bucket_location
